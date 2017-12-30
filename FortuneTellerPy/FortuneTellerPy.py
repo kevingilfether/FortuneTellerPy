566b053c1d1cd1bbe33c066df2bd9f0cdcc6e74a
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-30 07:49:57 -0500

    Added year and birthdate

--- FortuneTellerPy/FortuneTellerPy.py
+++ FortuneTellerPy/FortuneTellerPy.py
@@ -1,14 +1,19 @@
-def Quitter(input):
-    if input == "quit":
+def quitter(input):
+    if input.lower() == "quit":
         exit()
 
 #Part one - Copy and prompt
 print("I'm a fortune teller, of sorts.")
 first_name = input("What's your first name?\n")
-Quitter(first_name)
-print("\n")
+quitter(first_name)
+print()
 last_name = input("Good! Your last name?\n")
-Quitter(last_name)
+quitter(last_name)
+print()
+age = input("And your age?")
+print("You don't say! And what's your birth month (by number)?")
+birth_month = input("Like '1' for January, and so on.")
+
 
 
 
