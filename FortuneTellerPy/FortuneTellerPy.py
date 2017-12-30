7e7fa1d851f94b4bd709808df2703c196e5081bb
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-30 07:31:55 -0500

    Added Quitter function and call

--- FortuneTellerPy/FortuneTellerPy.py
+++ FortuneTellerPy/FortuneTellerPy.py
@@ -1,8 +1,16 @@
+def Quitter(input):
+    if input == "quit":
+        exit()
+
 #Part one - Copy and prompt
 print("I'm a fortune teller, of sorts.")
 first_name = input("What's your first name?\n")
-if first_name == "quit":
-    exit()
+Quitter(first_name)
 print("\n")
-last_name = input("Good! Your last name?")
+last_name = input("Good! Your last name?\n")
+Quitter(last_name)
+
+
+
+
 
