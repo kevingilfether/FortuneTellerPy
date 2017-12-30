88985d7239bbba99ab9b7ecaec3053229267930c
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-29 22:40:25 -0500

    Added quit for first name

--- FortuneTellerPy/FortuneTellerPy.py
+++ FortuneTellerPy/FortuneTellerPy.py
@@ -1,3 +1,8 @@
 #Part one - Copy and prompt
 print("I'm a fortune teller, of sorts.")
 first_name = input("What's your first name?\n")
+if first_name == "quit":
+    exit()
+print("\n")
+last_name = input("Good! Your last name?")
+
