3ac4f97316dc10012bf7a14565bf88cd1f5673c6
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-30 20:24:53 -0500

    Adding print lines and quitter

--- FortuneTellerPy/FortuneTellerPy.py
+++ FortuneTellerPy/FortuneTellerPy.py
@@ -10,11 +10,16 @@ print()
 last_name = input("Good! Your last name?\n")
 quitter(last_name)
 print()
-age = input("And your age?")
+age = input("And your age?\n")
+quitter(age)
+print()
 print("You don't say! And what's your birth month (by number)?")
-birth_month = input("Like '1' for January, and so on.")
+birth_month = input("Like '1' for January, and so on.\n")
+quitter(birth_month)
+print()
 print("What is your favorite ROYGBIV color?")
-favorite_color = input("I can help if you type 'help'.")
+favorite_color = input("I can help if you type 'help'.\n")
+print()
 while favorite_color == "help":
     #help prompt for favorite colors, with extra prompt
     print("R = Red")
@@ -24,10 +29,13 @@ while favorite_color == "help":
     print("B = Blue")
     print("I = Indigo")
     print("V = Violet")
+    print()
     favorite_color = input("What is your favorite ROYGBIV color?\n")
 quitter(favorite_color)
-num_sibs = input("Okay, you are a trooper. One last question: how many siblings do you have?")
+num_sibs = input("Okay, you are a trooper. One last question: how many siblings do you have?\n")
 quitter(num_sibs)
+print()
+
 
 
 
